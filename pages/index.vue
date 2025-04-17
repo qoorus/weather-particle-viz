e38df57c0492c40fc04b5f5922972bd40ea784e3
qoorus <q7254task@gmail.com>
2025-04-17 21:47:55 +0900

    トップページを作成し、そこに京都の天気を表示

---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { getCurrentWeatherFromCityName } from '~/composables/useWeatherAPI'
+
+const cityName = 'Kyoto'
+const weatherState = await getCurrentWeatherFromCityName(cityName)
+const weatherStateMain = weatherState.stateMain
+const weatherStateDescription = weatherState.stateDescription
+
+</script>
+
+<template>
+    <div class="container">
+        <h1>京都の今の天気はー？</h1>
+        <p>天気：{{ weatherStateMain }}</p>
+        <p>詳細：{{ weatherStateDescription }}</p>
+    </div>
+</template>
