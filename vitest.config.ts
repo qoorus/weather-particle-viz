ed0a1a9837af01529162937ea06c69f7bdd52b08
qoorus <q7254task@gmail.com>
2025-04-17 18:32:07 +0900

    テスト対象のファイルを設定

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,9 @@
-import vue from '@vitejs/plugin-vue'
 import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
   test: {
     environment: 'nuxt',
-    globals: true
-  }
+    // テスト対象ファイル
+    include: ['tests/init/*.test.ts'],
+  },
 })
