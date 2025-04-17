0fc67c505504f4025ca0bdfabbc0a9d74e8b6f0f
qoorus <q7254task@gmail.com>
2025-04-17 17:07:38 +0900

    都市名を入力したら、そこの緯度と経度を返す関数を作成

---
+++ composables/useWeatherAPI.ts
@@ -0,0 +1,29 @@
+const config = useRuntimeConfig()
+const apiKey = config.public.apiKey
+
+const baseUrlGeocoding = 'https://api.openweathermap.org/geo/1.0/direct'
+const baseUrlCurrentWeather = 'https://api.openweathermap.org/data/2.5/weather'
+
+async function getCoordinates(cityName: string): Promise<{lat: number, lon: number}> {
+    try {
+        const geoCoordinates = await useFetch(baseUrlGeocoding, {
+            params: {
+                q: cityName,
+                limit: 1,
+                appid: apiKey,
+            }
+        })
+        const data = await geoCoordinates.data.value
+        if (data && data.length > 0) {
+            return {
+                lat: data[0].lat,
+                lon: data[0].lon
+            }
+        } else {
+            throw new Error(`${cityName}の座標が見つかりませんでした`)
+        }
+    } catch (error) {
+        console.error('座標取得中にエラーが発生しました:', error)
+        throw error
+    }
+}
